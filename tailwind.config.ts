4173680863c209878823624647e5099f98633fb5
Caique Sobral <caiquelsobral@gmail.com>
2024-09-02 12:07:29 -0300

    STYLES: Add new tailwind reusable styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			backgroundColor: {
-				offWhite: "#FAF9F6",
-				primary: {
-					light: "#49698f",
-					normal: "#1C4473",
-					dark: "#16365c",
-				},
-			},
-			textColor: {
-				primary: "#1C4473",
-			},
-		},
-	},
-	plugins: [],
-};
-export default config;
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xsm: '350px',
+      },
+      backgroundColor: {
+        offWhite: '#FAF9F6',
+        primary: {
+          light: '#49698f',
+          normal: '#1C4473',
+          dark: '#16365c',
+        },
+      },
+      textColor: {
+        primary: '#1C4473',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config

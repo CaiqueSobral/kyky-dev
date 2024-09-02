db92d97d1380aa97e477b55ca7e462f35ec5b9fc
Caique Sobral <caiquelsobral@gmail.com>
2024-09-01 22:34:33 -0300

    FIX: Fix some tags typos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
-	return (
-		<header>
-			<span className="font-semibold text-xl text-[#1C4473]">Kyky.dev</span>
-		</header>
-	);
+  return (
+    <header>
+      <span className="font-bold text-xl text-[#1C4473]">Kyky.dev</span>
+    </header>
+  )
 }

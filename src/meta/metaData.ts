fec252a754fcb265caf1ce6b13887cc75c39434d
Caique Sobral <caiquelsobral@gmail.com>
2024-09-02 12:08:05 -0300

    FEAT: Add new metadata and viewport metrics

---
+++ src/meta/metaData.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from 'next'
+
+export const nextMetaData: Metadata = {
+  title: 'Kyky.dev',
+  description: "That's my cliche website",
+}
---
+++ src/meta/viewport.ts
@@ -0,0 +1,6 @@
+import type { Viewport } from 'next'
+
+export const nextViewport: Viewport = {
+  initialScale: 1.0,
+  width: 'device-width',
+}

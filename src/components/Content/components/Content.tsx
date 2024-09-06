8002515c2266feaba506168c0c256d0baff74f80
Caique Sobral <caiquelsobral@gmail.com>
2024-09-06 01:09:13 -0300

    FIX: Fix button transition

--- src/components/Content/components/Content.tsx
+++ src/components/Content/components/Content.tsx
@@ -14,7 +14,7 @@ export default function Content() {
         <div className="pop:after:-z-10 pop:after:-bottom-1 pop:after:-left-1 relative z-10 self-center pop:after:absolute pop:after:h-full pop:after:w-full pop:after:bg-accent pop:hover:after:bg-black">
           <a
             href="/files/Kyky-Resume.pdf"
-            className="inline-flex items-center gap-4 pop:rounded-none rounded-full bg-accent pop:bg-black px-6 py-1 pop:text-accent text-primary transition-all hover:scale-105 pop:hover:scale-100 hover:bg-accent-dark pop:hover:bg-accent pop:hover:text-black"
+            className="inline-flex items-center gap-4 pop:rounded-none rounded-full bg-accent pop:bg-black px-6 py-1 pop:text-accent text-primary transition-transform hover:scale-105 pop:hover:scale-100 hover:bg-accent-dark pop:hover:bg-accent pop:hover:text-black"
             target="_blank"
             rel="noreferrer"
           >

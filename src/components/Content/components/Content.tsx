cc08770df222f8f1b88ee73b68626fc27184857c
Caique Sobral <caiquelsobral@gmail.com>
2024-09-09 20:26:57 -0300

    FEAT: Add new url icon

--- src/components/Content/components/Content.tsx
+++ src/components/Content/components/Content.tsx
@@ -21,14 +21,18 @@ export default function Content() {
         <div className="pop:after:-z-10 pop:after:-bottom-1 pop:after:-left-1 relative z-10 self-center pop:after:absolute pop:after:h-full pop:after:w-full pop:after:bg-accent pop:hover:after:bg-black">
           <a
             href="/files/Kyky-Resume.pdf"
-            className="inline-flex items-center gap-4 pop:rounded-none rounded-full bg-accent pop:bg-black px-6 py-1 pop:text-accent text-primary transition-transform hover:scale-105 pop:hover:scale-100 hover:bg-accent-dark pop:hover:bg-accent pop:hover:text-black"
+            className="inline-flex items-center gap-4 pop:rounded-none rounded-full bg-accent pop:bg-black pop:stroke-accent stroke-primary px-6 py-1 pop:text-accent text-primary transition-transform hover:scale-105 pop:hover:scale-100 hover:bg-accent-dark pop:hover:bg-accent pop:hover:stroke-black pop:hover:text-black"
             target="_blank"
             rel="noreferrer"
           >
             <span className="pop:font-black text-sm md:text-base">
               My Resume
             </span>
-            <UrlIcon height={12} width={12} className="inline fill-primary" />
+            <UrlIcon
+              height={16}
+              width={16}
+              className="inline rotate-[135deg]"
+            />
           </a>
         </div>
       </div>

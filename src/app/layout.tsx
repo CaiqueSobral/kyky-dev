24d2a51ae1975469e9923f722657250dd211f93a
Caique Sobral <caiquelsobral@gmail.com>
2024-09-09 23:11:38 -0300

    CHORE: Add speed insights from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Source_Code_Pro } from 'next/font/google'
 import './globals.css'
 import { nextMetaData } from '@/meta/metaData'
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className="standard">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )

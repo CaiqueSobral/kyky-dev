3a47ffa394ce816e0710f122b57d79b5fc53b896
Caique Sobral <caiquelsobral@gmail.com>
2024-09-11 20:18:03 -0300

    REFAC: Use new menu inner button component

--- src/components/Header/MenuButton.tsx
+++ src/components/Header/MenuButton.tsx
@@ -5,6 +5,8 @@ import Down from '../icons/Down'
 import IconCoolButton from '../Custom/IconCoolButton'
 import About from '../icons/About'
 import Link from 'next/link'
+import MenuInnerButton from '../Custom/MenuInnerButton'
+import Home from '../icons/Home'
 
 export default function MenuButton() {
   const [active, setActive] = useState(false)
@@ -39,14 +41,19 @@ export default function MenuButton() {
           onMouseLeave={() => setNeeded(false)}
           className="pop:before:-z-10 absolute left-0 mt-1 pop:mt-2 flex flex-col std:rounded-lg bg-accent pop:bg-black std:bg-accent-dark py-2 pop:before:absolute pop:before:top-1 pop:before:right-1 pop:before:h-full pop:before:w-full pop:before:bg-accent"
         >
-          <Link
-            href={'/about'}
-            className="flex items-center gap-2 pop:bg-black std:bg-accent-dark pop:stroke-accent-light stroke-offWhite py-1 pr-4 pl-2 pop:font-medium pop:text-accent-light text-offWhite text-sm transition-all pop:hover:bg-accent std:hover:bg-accent-light pop:hover:stroke-black pop:hover:text-black"
-          >
-            <About className="h-[14px] pop:h-4 pop:w-4 w-[14px]" />
-            <span className="text-nowrap pop:font-medium pop:text-sm text-xs">
-              About me
-            </span>
+          <Link href={'/'} aria-label="Home">
+            <MenuInnerButton>
+              <Home className="h-[14px] pop:h-4 pop:w-4 w-[14px]" />
+              <span className="text-nowrap pop:font-medium text-sm">Home</span>
+            </MenuInnerButton>
+          </Link>
+          <Link href={'/about'} aria-label="About me">
+            <MenuInnerButton>
+              <About className="h-[14px] pop:h-4 pop:w-4 w-[14px]" />
+              <span className="text-nowrap pop:font-medium text-sm">
+                About me
+              </span>
+            </MenuInnerButton>
           </Link>
         </div>
       )}
